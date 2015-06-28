910dd638e59f2da736e00f7e4045241c6a114fdc
dominikus1993 <dominikus1910@gmail.com>
2015-06-28 21:56:41 +0200

    added 2 algorithms

---
+++ Factorial.fs
@@ -0,0 +1,10 @@
+﻿
+let rec factorial = function
+    |0 -> 1
+    |n -> factorial(n - 1) * n
+     
+[<EntryPoint>]
+let main argv =
+    let result = factorial 5
+    printfn "%A" result
+    0 // return an integer exit code
---
+++ Fib.fs
@@ -0,0 +1,11 @@
+﻿
+let rec fib = function
+    |0 -> 0
+    |1 -> 1
+    |n -> fib(n - 1) + fib(n - 2)
+     
+[<EntryPoint>]
+let main argv =
+    let result = fib 5
+    printfn "%A" result
+    0 // return an integer exit code
