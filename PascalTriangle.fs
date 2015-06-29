dd8ad0f227ee04e18c367ea5af615311295ed981
dominikus1993 <dominikus1910@gmail.com>
2015-06-29 22:59:57 +0200

    first commit

---
+++ PascalTriangle.fs
@@ -0,0 +1,12 @@
+﻿
+let rec pascalTriangle(n:int ,k:int)=
+    if n = k or k = 0
+        then 1
+    else 
+         pascalTriangle(n - 1, k) + pascalTriangle(n - 1, k - 1)
+     
+[<EntryPoint>]
+let main argv =
+    let result = pascalTriangle(3,2)
+    printfn "%A" result
+    0 // return an integer exit code
