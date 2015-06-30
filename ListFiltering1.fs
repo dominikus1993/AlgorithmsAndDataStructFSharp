20a46745564efaa5aa31a77439b5267676d7d38a
dominikus1993 <dominikus1910@gmail.com>
2015-06-30 08:45:03 +0200

    Add new chalelnge

---
+++ ListFiltering1.fs
@@ -0,0 +1,8 @@
+﻿
+let list = ['a';'b';'c';'d';'e']
+     
+[<EntryPoint>]
+let main argv =
+    let result = list|>List.filter(fun x -> x = 'd' || x = 'e')
+    printfn "%A" result
+    0 // return an integer exit code
