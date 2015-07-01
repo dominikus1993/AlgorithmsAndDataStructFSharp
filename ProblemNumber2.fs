10d6ecbae15893da168c9c38440ebec0b53714ab
dominikus1993 <dominikus1910@gmail.com>
2015-07-01 21:52:24 +0200

    add solution for three challenges with this site http://www.fssnip.net/an

---
+++ ProblemNumber2.fs
@@ -0,0 +1,9 @@
+﻿// count match items in the list for those defined char
+let list = [ 1..100 ]
+
+
+[<EntryPoint>]
+let main argv = 
+    let result = list |> List.rev |> List.tail |> List.head
+    printfn "%A" result 
+    0 // return an integer exit code
---
+++ ProblemNumber3.fs
@@ -0,0 +1,16 @@
+﻿// count match items in the list for those defined char
+let list = [ 1..100 ]
+
+let rec nth' (collection, n) = 
+    match collection,n with 
+   |[],_ -> 0
+   |x::_,1 -> x
+   |_::collection,n -> nth'(collection,(n-1)) 
+
+
+
+[<EntryPoint>]
+let main argv = 
+    let result = nth'(list, 6)
+    printfn "%A" result 
+    0 // return an integer exit code
