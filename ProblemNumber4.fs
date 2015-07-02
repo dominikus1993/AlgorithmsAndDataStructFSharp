6f008a77a19b8729ac17b54d24d1c1f76337ea7d
dominikus1993 <dominikus1910@gmail.com>
2015-07-02 20:39:08 +0200

    Added 3 new problems

---
+++ ProblemNumber4.fs
@@ -0,0 +1,7 @@
+﻿let list = [1..100]
+ 
+[<EntryPoint>]
+let main argv = 
+    let result = list |> List.sumBy(fun _ -> 1)
+    printfn "%A" result
+    0 // return an integer exit code
---
+++ ProblemNumber6.fs
@@ -0,0 +1,8 @@
+﻿let charArray text = [for i in text  ->  i] 
+let list = ["Visca el Barca"] |> List.collect(fun x -> charArray x)
+ 
+[<EntryPoint>]
+let main argv = 
+    let result = list |> List.rev 
+    printfn "%A" result
+    0 // return an integer exit code
