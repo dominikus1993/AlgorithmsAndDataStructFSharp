d713219e244f800789e7a61796993184f183fa1b
dominikus1993 <dominikus1910@gmail.com>
2015-07-11 17:24:11 +0200

    Add new solution

---
+++ JS/CountArithmeticSequenceInVector.js
@@ -0,0 +1,25 @@
+﻿(function() {
+
+    var data = [1, 2, 3, 8, 9, 10, 11, 14, 15, 16, 17];
+
+    var countSeq = function(list) {
+        var result = 0;
+        for (var i = 0; i < list.length - 2; i++) {
+            if (Math.abs(list[i] - list[i + 1]) === Math.abs(list[i + 2] - list[i + 1])) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    var main = function(list, func) {
+        return func(list);
+    }
+
+
+    console.log(main(data, countSeq));
+
+})();
+while (true) {
+    
+}
