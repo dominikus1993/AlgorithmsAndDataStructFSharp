5ff16bc3e5db2373254b25891848aa8885f38779
dominikus1993 <dominikus1910@gmail.com>
2015-07-12 19:51:46 +0200

    add problem nr. 7 in JavaScript

---
+++ JS/ProblemNumber7JS.js
@@ -0,0 +1,20 @@
+﻿
+(function() {
+    var data = [[1,2,3],[10,1,1],1,[1515,11]];
+    
+    var flatten = function(list) {
+        return list.reduce(function(x, y) {
+            return x.concat(y);
+        });
+    }
+
+    var main = function main(list,func) {
+        return func(data);
+    }
+
+    console.log(main(data, flatten));
+})();
+
+while (true) {
+    
+}
