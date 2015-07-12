80be8a27a3dc840c2fcd17ae2c0f516f41a6c8ab
dominikus1993 <dominikus1910@gmail.com>
2015-07-12 20:24:25 +0200

    Add two new solution in JavaScript

---
+++ JS/ListFiltering3.js
@@ -0,0 +1,33 @@
+﻿
+(function() {
+    var data = (function () {
+        var arr = [];
+        for (var i = 0; i < 100; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    })().filter(function(x) {
+        return x > 50;
+    });
+    
+    var sort = function(a, b) {
+        if (a % 2 === 0) {
+            return -1;
+        }
+        else if (b % 2 === 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    var main = function (list, func) {
+        return data.sort(func);
+    }
+
+    console.log(main(data, sort));
+})();
+
+while (true) {
+    
+}
---
+++ JS/ListFiltering5.js
@@ -0,0 +1,29 @@
+﻿
+(function() {
+    var data = (function() {
+        var arr = [];
+        for (var i = 0; i < 10; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    })();
+    Array.prototype.flatMap = function(x) {
+        return Array.prototype.concat.apply([], this.map(x));
+    }
+
+    var collect = function (list) {
+        return list.flatMap(function (x) {
+            return [x,x];
+        });
+    }
+
+    var main = function (list, func) {
+        return func(list);
+    }
+
+    console.log(main(data, collect));
+})();
+
+while (true) {
+    
+}
