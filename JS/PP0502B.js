4f453b759f3786ad7214785e0c9e8e732b23e5af
dominikus1993 <dominikus1910@gmail.com>
2015-07-16 21:23:07 +0200

    Add two new solution

---
+++ JS/PP0502B.js
@@ -0,0 +1,18 @@
+﻿
+(function () {
+
+    var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    var reverse = function(list) {
+        return list.reverse();
+    }
+    var main = function (list,func) {
+            return func(list);
+        }
+
+        console.log(main(data,reverse));
+})();
+
+while (true) {
+    
+}
---
+++ JS/isPrime.js
@@ -0,0 +1,21 @@
+﻿
+(function() {
+    var isPrime  = function isPrime(number, n) {
+        if (number === 1) {
+            return false;
+        } else if (n === Math.ceil(Math.sqrt(number))) {
+            return (number % n) === 0;
+        } else {
+            return isPrime(number, n - 1) || (number % n === 0);
+        }
+    }
+    var main = function (number,func) {
+            return !func(number, number - 1);
+        }
+
+        console.log(main(3,isPrime));
+})();
+
+while (true) {
+    
+}
