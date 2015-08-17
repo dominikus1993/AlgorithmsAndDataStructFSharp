0b6aaa6cd091ff067c59f6df25c79223d7eb08f5
Dominik Kotecki <dominikus1910@gmail.com>
2015-08-17 22:39:55 +0200

    Create ProjectEuler20.js

---
+++ JS/ProjectEuler20.js
@@ -0,0 +1,28 @@
+
+var Euler20;
+Euler20 = (function() {
+    function Euler20() {
+        var that = this;
+        this.factorial = function factorial(n) {
+            if (n == 0) {
+                return 1;
+            } else {
+                return factorial(n - 1) * n;
+            }
+        }
+        this.getResult = function(num) {
+            var res = that.factorial(num);
+            return parseFloat(res);
+        }
+    }
+
+    return Euler20;
+})();
+
+var mySeq = new Euler20().getResult(100).split("").map(function(x) {
+    return parseInt(x, 10);
+}).reduce(function(x, y) {
+    return x + y;
+});
+console.log(mySeq);
+while (true) { }
